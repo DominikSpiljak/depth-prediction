Fixed visualisation and added normalization

data/data_module.py
@@ -7,23 +7,29 @@ from data.loader import DataMatLoader
 
 
 class DepthEstimationDataset(Dataset):
-    def __init__(self, loader, indices, transformations):
+    def __init__(self, loader, indices, rgb_transformation, depth_transformation):
         self.loader = loader
         self.indices = indices
-        self.transformations = transformations
+        self.rgb_transformation = rgb_transformation
+        self.depth_transformation = depth_transformation
 
     def __len__(self):
-        return len(indices)
+        return len(self.indices)
 
     def __getitem__(self, idx):
         rgb_image, depth_map = self.loader[self.indices[idx]]
-        return transformations(rgb_image), transformations(depth_map.copy())
+        return (
+            self.indices[idx],
+            self.rgb_transformation(rgb_image.copy()),
+            self.depth_transformation(depth_map.copy()),
+        )
 
 
-def collate_fn(self, batch):
-    rgb_images = torch.stack([sample[0] for sample in batch])
-    depth_maps = torch.stack([sample[1] for sample in batch])
-    return rgb_images, depth_maps
+def collate_fn(batch):
+    indices = [sample[0] for sample in batch]
+    rgb_images = torch.stack([sample[1] for sample in batch])
+    depth_maps = torch.stack([sample[2] for sample in batch])
+    return indices, rgb_images, depth_maps
 
 
 class DepthEstimationDataModule(pl.LightningDataModule):
@@ -34,22 +40,30 @@ class DepthEstimationDataModule(pl.LightningDataModule):
         batch_size: int = 2,
         test_ratio: float = 0.2,
         val_ratio=0.1,
+        num_workers=4,
         image_size=[240, 320],
     ):
         self.data_path = data_path
         self.batch_size = batch_size
-        self.transform = transforms.Compose(
-            [transforms.ToTensor(), transforms.Resize(image_size)]
-        )
+        self.num_workers = num_workers
+        base_transform = [transforms.ToTensor(), transforms.Resize(image_size)]
+        self.rgb_transform = [
+            *base_transform,
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+        self.depth_transform = [
+            *base_transform,
+            transforms.Normalize((5), (5)),
+        ]
         self.val_ratio = val_ratio
         self.test_ratio = test_ratio
 
     def setup(self, stage):
-        loader = DataMatLoader(data_path)
+        loader = DataMatLoader(self.data_path)
 
         num_samples = len(loader)
-        num_val_samples = int(num_samples * val_ratio)
-        num_test_samples = int(num_samples * test_ratio)
+        num_val_samples = int(num_samples * self.val_ratio)
+        num_test_samples = int(num_samples * self.test_ratio)
         indices = torch.arange(num_samples)
 
         num_train_samples_ = num_samples - num_val_samples
@@ -59,21 +73,30 @@ class DepthEstimationDataModule(pl.LightningDataModule):
             generator=torch.Generator().manual_seed(42),
         )
 
-        num_train_samples = num_train_samples_ - num_val_samples
+        num_train_samples = num_train_samples_ - num_test_samples
         train_indices, test_indices = random_split(
-            torch.arange(train_indices_),
+            train_indices_,
             [num_train_samples, num_test_samples],
             generator=torch.Generator().manual_seed(42),
         )
 
         self.train_dataset = DepthEstimationDataset(
-            loader=loader, indices=train_indices, transformation=self.transform
+            loader=loader,
+            indices=train_indices,
+            rgb_transformation=transforms.Compose(self.rgb_transform),
+            depth_transformation=transforms.Compose(self.depth_transform),
         )
         self.val_dataset = DepthEstimationDataset(
-            loader=loader, indices=val_indices, transformation=self.transform
+            loader=loader,
+            indices=val_indices,
+            rgb_transformation=transforms.Compose(self.rgb_transform),
+            depth_transformation=transforms.Compose(self.depth_transform),
         )
         self.test_dataset = DepthEstimationDataset(
-            loader=loader, indices=test_indices, transformation=self.transform
+            loader=loader,
+            indices=test_indices,
+            rgb_transformation=transforms.Compose(self.rgb_transform),
+            depth_transformation=transforms.Compose(self.depth_transform),
         )
 
     def train_dataloader(self):
@@ -81,6 +104,7 @@ class DepthEstimationDataModule(pl.LightningDataModule):
             self.train_dataset,
             batch_size=self.batch_size,
             collate_fn=collate_fn,
+            num_workers=self.num_workers,
             shuffle=True,
             pin_memory=True,
         )
@@ -90,6 +114,7 @@ class DepthEstimationDataModule(pl.LightningDataModule):
             self.val_dataset,
             batch_size=self.batch_size,
             collate_fn=collate_fn,
+            num_workers=self.num_workers,
             pin_memory=True,
         )
 
@@ -98,5 +123,6 @@ class DepthEstimationDataModule(pl.LightningDataModule):
             self.test_dataset,
             batch_size=self.batch_size,
             collate_fn=collate_fn,
+            num_workers=self.num_workers,
             pin_memory=True,
         )

data/visualiser.py
@@ -2,7 +2,8 @@ import cv2 as cv
 import numpy as np
 
 
-def visualise_depth(depth_map, rgb_im=None):
+def visualise_depth(*, depth_map, rgb_im=None):
+    depth_map = (depth_map + 1) / 2
     min_val = depth_map.min()
     max_val = depth_map.max()
     if min_val == max_val:
@@ -12,8 +13,24 @@ def visualise_depth(depth_map, rgb_im=None):
     depth_color_map = cv.applyColorMap(
         (shifted_map * 255).astype(np.uint8), cv.COLORMAP_JET
     )
-
     if rgb_im is None:
         return depth_color_map
+
+    rgb_im = (rgb_im + 1) / 2
+    rgb_im = (rgb_im * 255).astype(np.uint8)
+
+    print(
+        depth_color_map.shape,
+        depth_color_map.min(),
+        depth_color_map.max(),
+        depth_color_map.dtype,
+    )
+
+    print(
+        rgb_im.shape,
+        rgb_im.min(),
+        rgb_im.max(),
+        rgb_im.dtype,
+    )
 
     return np.hstack((rgb_im, depth_color_map))